1730521a2dc9e4bda9027a1dd89635680f1106e6
Yanova <riefkyanova@gmail.com>
2024-03-25 12:57:34 +0700

    array

--- script.js
+++ script.js
@@ -53,26 +53,51 @@
 // console.log(endTotal(2, 3));
 
 
-const calcAvg = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+// const calcAvg = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 
-function checkWinner(avg1, avg2){
-    if (avg1 >= 2 * avg2){
-        return `dolphins win (${avg1} vs. ${avg2})`;
-    }
-    else if (avg2 >= 2 * avg1){
-        return `koalas win (${avg2} vs. ${avg1})`;
-    }
-    else {
-        return `no team wins`;
-    }
+// function checkWinner(avg1, avg2){
+//     if (avg1 >= 2 * avg2){
+//         return `dolphins win (${avg1} vs. ${avg2})`;
+//     }
+//     else if (avg2 >= 2 * avg1){
+//         return `koalas win (${avg2} vs. ${avg1})`;
+//     }
+//     else {
+//         return `no team wins`;
+//     }
 
-}
+// }
 
-const dolphin1 = calcAvg(44, 23, 71);
-const koalas1 = calcAvg(65, 54, 49);
+// const dolphin1 = calcAvg(44, 23, 71);
+// const koalas1 = calcAvg(65, 54, 49);
 
-const dolphin2 = calcAvg(85, 54, 41);
-const koalas2 = calcAvg(23, 34, 27);
+// const dolphin2 = calcAvg(85, 54, 41);
+// const koalas2 = calcAvg(23, 34, 27);
 
-console.log(checkWinner(dolphin1, koalas1));
-console.log(checkWinner(dolphin2, koalas2));
+// console.log(checkWinner(dolphin1, koalas1));
+// console.log(checkWinner(dolphin2, koalas2));
+
+/// array
+
+const food = ['pizza', 'burger', 'soda', 'juice'];
+console.log(food);
+console.log(food[1]);
+
+console.log(food.length);
+food[2] = 'water';
+console.log(food);
+
+food.push('fries');
+console.log(food);
+
+food.unshift('bread');
+console.log(food);
+
+food.pop();
+console.log(food);
+
+food.shift();
+console.log(food);
+
+console.log(food.indexOf('burger'));
+console.log(food.includes('water'));
